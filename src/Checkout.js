4ec0d702d73b94e4ad05103ac5f6a905a48de3e0
Evrydiki <evridikitsa@gmail.com>
2021-07-08 23:57:38 +0300

    Update Checkout.js

--- src/Checkout.js
+++ src/Checkout.js
@@ -3,17 +3,30 @@ import { useStateValue } from "./StateProvider";
 import "./Checkout.css";
 
 function Checkout() {
-    const [{ basket }] = useStateValue();
+  const [{ basket }] = useStateValue();
 
-    return (
-        <div className="checkout">
-            <img
-                className="checkout__ad"
-                src="https:images-na.ssl-images-amazon.com/images/"
-                alt=""
-            />
+  return (
+    <div className="checkout">
+      <img
+        className="checkout__ad"
+        src="https:images-na.ssl-images-amazon.com/images/"
+        alt=""
+      />
+      {basket.length === 0 ? (
+        <div>
+          <h2>Your Shopping Basket is empty</h2>
+          <p>
+            You have no items in your basket. To buy one or "Add to basket" next
+            to the item.
+          </p>
         </div>
-    )
+      ) : (
+        <div>
+          <h2>Your Shopping Basket</h2>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Checkout;
